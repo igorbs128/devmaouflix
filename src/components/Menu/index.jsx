a139dc23d45a385d784c5be170361ee866c1cd40
igorbs128 <50889133+igorbs128@users.noreply.github.com>
2020-07-28 21:40:03 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from '../../assets/img/MaouFlix.png'
+import './Menu.css';
+//import ButtonLink from '../Buttons';
+import Button from '../Button';
+
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+            <img className="Logo" src={Logo} alt='MaouFlix' />
+            </a>
+
+            <Button as='a' className="ButtonLink" href="/">
+                Novo Vídeo
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
